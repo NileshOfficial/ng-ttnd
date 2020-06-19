2b889d1a9edc294ec1ad98f9c9c74232d18c22d9
livingscripts <nilesh22.a67@gmail.com>
2020-06-20 03:34:39 +0530

    buzz-post layout complete

--- src/app/components/buzz/buzzpost/buzzpost.component.ts
+++ src/app/components/buzz/buzzpost/buzzpost.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ttnd-buzzpost',
   templateUrl: './buzzpost.component.html',
-  styleUrls: ['./buzzpost.component.css']
+  styleUrls: ['./buzzpost.component.css', '../../common.css']
 })
 export class BuzzPostComponent implements OnInit {
+
+  liked: boolean = true;
+  disliked: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleLike(): void {
+    this.liked = !this.liked;
+  }
+  
+  toggleDislike(): void {
+    this.disliked = !this.disliked;
   }
 
 }
