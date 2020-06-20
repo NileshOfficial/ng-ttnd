cab43c0907a007dea1dad1dd5646b9da7784d745
livingscripts <nilesh22.a67@gmail.com>
2020-06-20 18:14:58 +0530

    comment section comment threading remains

--- src/app/components/buzz/commentsSection/comment/comment.component.ts
+++ src/app/components/buzz/commentsSection/comment/comment.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ttnd-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css', '../../../common.css']
 })
 export class CommentComponent implements OnInit {
 
