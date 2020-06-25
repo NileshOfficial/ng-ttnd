bd9db9463077edb4727b8c4bf4f6099c460d5861
livingscripts <nilesh22.a67@gmail.com>
2020-06-25 12:15:42 +0530

    login config

---
+++ src/app/config/login.conf.ts
@@ -0,0 +1,10 @@
+export const CLIENT_ID = '1092418005405-igjkksf5ap5vit1d6grhsolnnnu3sd3h.apps.googleusercontent.com';
+export const OAUTH_GRANT_CODE_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
+export const REDIRECT_URI = 'http://localhost:4200/auth';
+export const SCOPE = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+];
+export const RESPONSE_TYPE = 'code';
+export const ACCESS_TYPE = 'offline';
+export const PROMPT = 'consent';
