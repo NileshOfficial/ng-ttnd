61140c82fbf5655b0443293a17cab1fa3ef0165b
livingscripts <nilesh22.a67@gmail.com>
2020-06-25 16:21:43 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthCallbackComponent } from './components/login-board/auth-callback/auth-callback.component';
+import { LoginBoardComponent } from './components/login-board/login-board.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+	{
+		path: 'auth',
+		children: [
+			{ path: '', component: AuthCallbackComponent },
+			{ path: 'login', component: LoginBoardComponent }
+		]
+  },
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
