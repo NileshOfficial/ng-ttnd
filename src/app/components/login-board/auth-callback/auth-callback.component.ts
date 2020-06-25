898f446e9e5cb0f1054732b9efb839b4aa607a05
livingscripts <nilesh22.a67@gmail.com>
2020-06-25 13:47:42 +0530

    removed localstore service

--- src/app/components/login-board/auth-callback/auth-callback.component.ts
+++ src/app/components/login-board/auth-callback/auth-callback.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginDataService } from '../../../services/dataServices/auth.service';
 
 @Component({
-  selector: 'ttnd-auth-callback',
-  templateUrl: './auth-callback.component.html',
-  styleUrls: ['./auth-callback.component.css', '../../common.css']
+	selector: 'ttnd-auth-callback',
+	templateUrl: './auth-callback.component.html',
+	styleUrls: ['./auth-callback.component.css', '../../common.css']
 })
 export class AuthCallbackComponent implements OnInit {
+	constructor(private logindata: LoginDataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/services/dataServices/auth.service.ts
+++ src/app/services/dataServices/auth.service.ts
@@ -1,46 +1,46 @@
 import { Injectable } from '@angular/core';
 import { LoginToken } from '../../models/token.model';
 
+const storeToken = (token: LoginToken): void => {
+	localStorage.setItem('token', JSON.stringify(token));
+};
+
+const retrieveToken = (): LoginToken | null => {
+	const token = localStorage.getItem('token');
+	return token ? JSON.parse(token) : null;
+};
+
+const deleteToken = (): void => {
+	localStorage.clear();
+};
+
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class LoginDataService {
+	private mode: string = null;
+	private token: LoginToken = null;
 
-  private mode: string = null;
-  private token: LoginToken = null;
+	get loginMode(): string {
+		return this.mode;
+	}
 
-  get loginMode(): string {
-    return this.mode;
-  }
+	set loginMode(mode: string) {
+		this.loginMode = mode;
+	}
 
-  set loginMode(mode: string) {
-    this.loginMode = mode;
-  }
+	get loginToken(): LoginToken {
+		if (!this.token) this.token = retrieveToken();
+		return this.token;
+	}
 
-  get loginToken(): LoginToken {
-    return this.token;
-  }
-
-  set loginToken(token: LoginToken) {
+	set loginToken(token: LoginToken) {
     this.token = token;
+    storeToken(this.token);
   }
 
-}
-
-@Injectable({
-  providedIn: 'root'
-})
-export class LocalStoreInterface {
-  storeToken(token: LoginToken): void {
-    localStorage.setItem('token', JSON.stringify(token));
-  }
-
-  retrieveToken(): LoginToken | null {
-    const token = localStorage.getItem('token');
-    return token ? JSON.parse(token) : null;
-  }
-
-  deleteToken(): void {
-    localStorage.clear();
+  delete(): void {
+    this.token = null;
+    deleteToken();
   }
 }
