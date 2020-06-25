da9d8cdb458baa32dbde88b1dab6117cf4264ebe
livingscripts <nilesh22.a67@gmail.com>
2020-06-25 19:59:50 +0530

    setup with router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthCallbackComponent } from './components/login-board/auth-callback/auth-callback.component';
 import { LoginBoardComponent } from './components/login-board/login-board.component';
 import { HomeComponent } from './components/home/home.component';
+import { BuzzComponent } from './components/buzz/buzz.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+	{ path: '', redirectTo: '/auth/login', pathMatch: 'full' },
 	{
 		path: 'auth',
 		children: [
 			{ path: '', component: AuthCallbackComponent },
 			{ path: 'login', component: LoginBoardComponent }
 		]
-  },
-  { path: 'home', component: HomeComponent}
+	},
+	{
+		path: 'home',
+		component: HomeComponent,
+		children: [{ path: 'buzz', component: BuzzComponent }]
+	}
 ];
 
 @NgModule({
