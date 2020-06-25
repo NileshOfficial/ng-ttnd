92ab4006db5637d71fd9a37a6ed57451c7308dc7
livingscripts <nilesh22.a67@gmail.com>
2020-06-25 19:24:27 +0530

    added resolve, super and my profile nav links

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ttnd-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css', '../common.css']
 })
 export class NavComponent implements OnInit {
   personalNotes: Array<string> = ["notes 1", "notes 2", "notes 3"];
