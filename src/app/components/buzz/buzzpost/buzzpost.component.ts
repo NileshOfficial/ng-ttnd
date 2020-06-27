e2b2e7ad1422011b6c99c6432620b11accca8b9d
livingscripts <nilesh22.a67@gmail.com>
2020-06-27 22:50:42 +0530

    showing durations with buzz post

--- src/app/components/buzz/buzzpost/buzzpost.component.ts
+++ src/app/components/buzz/buzzpost/buzzpost.component.ts
@@ -2,57 +2,53 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BUZZ_POST, PROFILE_PIC } from '../../../config/uri.conf';
 
 interface Post {
-  _id: string;
-  category: string;
-  data: number;
-  description: string;
-  liked: boolean;
-  likes: number;
-  disliked: boolean;
-  dislikes: number;
-  email: string;
-  images: Array<string>;
-  title: string;
-  user: {
-    email: string;
-    name: string;
-    picture: string;
-  }
+	_id: string;
+	category: string;
+	date: number;
+	description: string;
+	liked: boolean;
+	likes: number;
+	disliked: boolean;
+	dislikes: number;
+	email: string;
+	images: Array<string>;
+	title: string;
+	user: {
+		email: string;
+		name: string;
+		picture: string;
+	};
 }
 
 @Component({
-  selector: 'ttnd-buzzpost',
-  templateUrl: './buzzpost.component.html',
-  styleUrls: ['./buzzpost.component.css', '../../common.css']
+	selector: 'ttnd-buzzpost',
+	templateUrl: './buzzpost.component.html',
+	styleUrls: ['./buzzpost.component.css', '../../common.css']
 })
 export class BuzzPostComponent implements OnInit {
+	@Input() data: Post;
 
-  @Input() data: Post;
+	liked: boolean = true;
+	disliked: boolean = true;
+	previewImages: Array<string> = [];
 
-  liked: boolean = true;
-  disliked: boolean = true;
+	constructor() {}
 
-  previewImages: Array<string> = [];
+	ngOnInit(): void {
+		const images = this.data.images;
+		this.data.images = images.map((image) => [BUZZ_POST, image].join('/'));
+		this.previewImages = images.length >= 4 ? this.data.images.slice(0, 5) : this.data.images;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.data);
-    const images = this.data.images;
-    this.data.images = images.map(image => [BUZZ_POST, image].join('/'));
-    this.previewImages = images.length >= 4 ? this.data.images.slice(0, 5) : this.data.images;
-
-    this.data.user.picture = [PROFILE_PIC, this.data.user.picture].join('/');
-    this.liked = this.data.liked;
+		this.data.user.picture = [PROFILE_PIC, this.data.user.picture].join('/');
+		this.liked = this.data.liked;
     this.disliked = this.data.disliked;
   }
 
-  toggleLike(): void {
-    this.liked = !this.liked;
-  }
+	toggleLike(): void {
+		this.liked = !this.liked;
+	}
 
-  toggleDislike(): void {
-    this.disliked = !this.disliked;
-  }
-
+	toggleDislike(): void {
+		this.disliked = !this.disliked;
+	}
 }
