e36105bb0a1e3894f0b6fa0d8a41dbbeeee01eab
livingscripts <nilesh22.a67@gmail.com>
2020-06-27 17:31:43 +0530

    uris to fetch user profile pic, buzz post pics and complaint documents

--- src/app/config/uri.conf.ts
+++ src/app/config/uri.conf.ts
@@ -5,3 +5,6 @@ const getEndpoint = (path: string): string => [SERVER_DOMAIN, path].join('/');
 export const SIGN_UP: string = getEndpoint('auth/signup');
 export const SIGN_IN: string = getEndpoint('auth/signin');
 export const BUZZ: string = getEndpoint('buzz');
+export const PROFILE_PIC: string = getEndpoint('profile');
+export const BUZZ_POST: string = getEndpoint('post');
+export const COMPLAINT_POST: string = getEndpoint('complaint')
