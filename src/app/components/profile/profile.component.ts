75533073a9717ea62df935b420b64c5bc0a2d480
livingscripts <nilesh22.a67@gmail.com>
2020-06-28 23:11:27 +0530

    edit profile form layout done

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'ttnd-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css', '../common.css']
+	selector: 'ttnd-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css', '../common.css']
 })
 export class ProfileComponent implements OnInit {
+  editProfileForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
