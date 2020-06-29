903e3a2d5e703df46aca1f0ef5e49c880bbeb7c3
livingscripts <nilesh22.a67@gmail.com>
2020-06-29 19:53:55 +0530

    added profile and activity tabs

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginDataService } from 'src/app/services/dataServices/auth.service';
 
 @Component({
-  selector: 'ttnd-profile-page',
-  templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css', '../common.css']
+	selector: 'ttnd-profile-page',
+	templateUrl: './profile-page.component.html',
+	styleUrls: ['./profile-page.component.css', '../common.css']
 })
 export class ProfilePageComponent implements OnInit {
+	editableProfile: boolean = false;
+	userEmail: string = '';
+	currentView: string = 'profile';
 
-  constructor() { }
+	constructor(private router: Router, private authData: LoginDataService) {
+		const userEmail = this.router.getCurrentNavigation().extras.state;
+		if (userEmail) this.userEmail = userEmail.email;
+		else router.navigate(['/home']);
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.editableProfile = (this.userEmail === this.authData.idTokenData().email);
+	}
 
+	showProfile(): void {
+		this.currentView = 'profile';
+	}
+
+	showActivity(): void {
+		this.currentView = 'activity';
+	}
 }
