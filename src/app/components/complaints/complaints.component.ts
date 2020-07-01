39368fb47442054e55d5d89ed90e2a0eea22f46d
livingscripts <nilesh22.a67@gmail.com>
2020-07-01 23:29:49 +0530

    complaint posting completed

--- src/app/components/complaints/complaints.component.ts
+++ src/app/components/complaints/complaints.component.ts
@@ -1,20 +1,112 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DepartmentapiService } from 'src/app/services/apis/departmentapi.service';
+import { Department } from 'src/app/models/department.model';
+import { ComplaintapiService } from 'src/app/services/apis/complaintapi.service';
 
 @Component({
-  selector: 'ttnd-complaints',
-  templateUrl: './complaints.component.html',
-  styleUrls: ['./complaints.component.css', '../common.css']
+	selector: 'ttnd-complaints',
+	templateUrl: './complaints.component.html',
+	styleUrls: ['./complaints.component.css', '../common.css']
 })
 export class ComplaintsComponent implements OnInit {
+	complaintForm: FormGroup = null;
+	departmentList: Array<Department> = [];
+	allowedFileTypes: Array<string> = ['image/jpeg', 'image/png', 'text/plain', 'application/pdf'];
+	uploadedFiles: Array<File> = [];
 
-  complaintForm: FormGroup = null;
-  constructor() { }
+	invalidFile: boolean = false;
+	postingComplaint: boolean = false;
+	err: boolean = false;
 
-  ngOnInit(): void {
-    this.complaintForm = new FormGroup({
+	constructor(private complaintApi: ComplaintapiService, private deptApi: DepartmentapiService) {}
 
-    });
-  }
+	ngOnInit(): void {
+		this.deptApi.getDepartments().subscribe(
+			(data) => {
+				this.departmentList = data;
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
 
+		this.complaintForm = new FormGroup({
+			department: new FormControl('', [Validators.required]),
+			title: new FormControl('', [Validators.required]),
+			description: new FormControl('', [Validators.required])
+		});
+	}
+
+	createComplaint() {
+		if (this.complaintForm.valid && !this.invalidFile) {
+			this.showLoader();
+			const data = this.prepareDataToPost();
+			this.complaintApi.postComplaint(data).subscribe(
+				(data) => {
+          console.log(data);
+					this.complaintForm.reset({
+						department: '',
+						title: ''
+					});
+					this.hideLoader();
+				},
+				(err) => {
+					console.log(err);
+					this.showErr();
+				}
+			);
+		}
+	}
+
+	fileChange(event) {
+		this.invalidFile = false;
+		const files: Array<File> = event.target.files;
+		if (files.length > 10) this.invalidFile = true;
+
+		for (const file of files) {
+			if (!this.allowedFileTypes.includes(file.type) || file.size >= 5 * 1024 * 1024) {
+				this.invalidFile = true;
+				break;
+			}
+		}
+		this.uploadedFiles = files;
+	}
+
+	prepareDataToPost(): FormData {
+		const sharableFormData = new FormData();
+
+		const formData = this.complaintForm.value;
+		const fields = Object.entries(formData);
+
+		for (const field of fields) {
+			if (field[1]) {
+				sharableFormData.append(field[0], field[1] as string);
+			}
+		}
+
+		if (this.uploadedFiles.length > 0)
+			for (const file of this.uploadedFiles) sharableFormData.append('files', file, file.name);
+
+		return sharableFormData;
+	}
+
+	showLoader(): void {
+		this.postingComplaint = true;
+	}
+
+	showErr(): void {
+		this.postingComplaint = false;
+		this.err = true;
+	}
+
+	hideLoader(): void {
+		this.postingComplaint = false;
+		this.err = false;
+	}
+
+	reset() {
+		this.invalidFile = false;
+		this.uploadedFiles = [];
+	}
 }
