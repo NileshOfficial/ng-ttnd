ba728146864d0b91e47af4e30d9d31e8825b7f28
livingscripts <nilesh22.a67@gmail.com>
2020-07-01 16:28:04 +0530

    added activity to profile page

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginDataService } from 'src/app/services/dataServices/auth.service';
+import { BuzzapiService } from 'src/app/services/apis/buzzapi.service';
+import { BuzzPostComponent } from '../buzz/buzzpost/buzzpost.component';
 
 @Component({
 	selector: 'ttnd-profile-page',
@@ -8,18 +10,28 @@ import { LoginDataService } from 'src/app/services/dataServices/auth.service';
 	styleUrls: ['./profile-page.component.css', '../common.css']
 })
 export class ProfilePageComponent implements OnInit {
+	readonly buzzPostComponent = BuzzPostComponent;
 	editableProfile: boolean = false;
 	userEmail: string = '';
 	currentView: string = 'profile';
+	buzzDataBindings: any = null;
+	buzzFilter: any = {}
 
-	constructor(private router: Router, private authData: LoginDataService) {
+	constructor(private router: Router, private authData: LoginDataService, public buzzApi: BuzzapiService) {
 		const userEmail = this.router.getCurrentNavigation().extras.state;
 		if (userEmail) this.userEmail = userEmail.email;
 		else router.navigate(['/home']);
 	}
 
 	ngOnInit(): void {
-		this.editableProfile = (this.userEmail === this.authData.idTokenData().email);
+		const allowEditing = (this.userEmail === this.authData.idTokenData().email);
+		this.editableProfile = allowEditing
+		this.buzzDataBindings = {
+			editable: allowEditing
+		};
+		this.buzzFilter = {
+			email: this.userEmail
+		}
 	}
 
 	showProfile(): void {
