ef5a00630f29087a81fd94515bc3e690b4d863ba
livingscripts <nilesh22.a67@gmail.com>
2020-07-02 23:50:29 +0530

    complaint card shows dynamic data

--- src/app/components/complaints/complaint/complaint.component.ts
+++ src/app/components/complaints/complaint/complaint.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ComplaintapiService } from 'src/app/services/apis/complaintapi.service';
+import { Complaint } from 'src/app/models/complaint.model';
 
 @Component({
-  selector: 'ttnd-complaint',
-  templateUrl: './complaint.component.html',
-  styleUrls: ['./complaint.component.css', '../../common.css']
+	selector: 'ttnd-complaint',
+	templateUrl: './complaint.component.html',
+	styleUrls: ['./complaint.component.css', '../../common.css']
 })
 export class ComplaintComponent implements OnInit {
+	@Input() editableBy: string = 'user'; /* admin, user, view */
+  @Input() data: Complaint = null;
 
-  editableBy: string = 'user'; /* admin, user, view */
+	constructor(private complaintApi: ComplaintapiService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.complaintApi.get().subscribe(
+			(data) => {
+				console.log(data);
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
+	}
 }
