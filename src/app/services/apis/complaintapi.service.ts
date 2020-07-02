264db5ee031136ef4c39e9861302eaffcf2657f9
livingscripts <nilesh22.a67@gmail.com>
2020-07-02 23:42:29 +0530

    added get method

--- src/app/services/apis/complaintapi.service.ts
+++ src/app/services/apis/complaintapi.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { COMPLAINT } from '../../config/uri.conf';
+import { Complaint } from 'src/app/models/complaint.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,7 +10,11 @@ import { COMPLAINT } from '../../config/uri.conf';
 export class ComplaintapiService {
 	constructor(private http: HttpClient) {}
 
-	postComplaint(data: any): Observable<{ inserted: boolean, referenceToken: string }> {
-    return this.http.post<{ inserted: boolean, referenceToken: string }>(COMPLAINT, data);
-  }
+	get(query: any = {}, skip: number = 0, limit: number = 0): Observable<Array<Complaint>> {
+		return this.http.get<Array<Complaint>>(COMPLAINT);
+	}
+
+	postComplaint(data: any): Observable<{ inserted: boolean; referenceToken: string }> {
+		return this.http.post<{ inserted: boolean; referenceToken: string }>(COMPLAINT, data);
+	}
 }
