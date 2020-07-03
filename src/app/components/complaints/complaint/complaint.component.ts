bc710d09e558468d9f68e18f875d28ea60ce80a8
livingscripts <nilesh22.a67@gmail.com>
2020-07-03 19:54:14 +0530

    update estimated time complete

--- src/app/components/complaints/complaint/complaint.component.ts
+++ src/app/components/complaints/complaint/complaint.component.ts
@@ -13,7 +13,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ComplaintComponent implements OnInit {
 	@ViewChild('statusDropdown') statusDropdown: ElementRef;
 
-	@Input() editableBy: string = 'admin'; /* admin, user, view */
+	@Input() editableBy: string = 'user'; /* admin, user, view */
 	@Input() data: Complaint = null;
 	@Output() reload: EventEmitter<boolean> = new EventEmitter();
 
@@ -24,18 +24,11 @@ export class ComplaintComponent implements OnInit {
 	showEditForm: boolean = false;
 	showEstimatedTimeForm: boolean = false;
 	updatingStatus: boolean = false;
+	updatingStatusErr: boolean = false;
 
 	constructor(private complaintApi: ComplaintapiService, private deptApi: DepartmentapiService) {}
 
 	ngOnInit(): void {
-		const defaultSpanValue: number = this.data.status === 'in progress' ? this.data.estimatedTime.value : null;
-		const defaultSpanType: string = this.data.status === 'in progress' ? this.data.estimatedTime.spanType : '';
-
-		this.estimatedTimeForm = new FormGroup({
-			value: new FormControl(defaultSpanValue, [Validators.required, Validators.min(1)]),
-			spanType: new FormControl(defaultSpanType, [Validators.required, this.verifySpanType.bind(this)])
-		});
-
 		this.deptApi.getDepartments().subscribe(
 			(data) => {
 				this.departmentList = data;
@@ -61,6 +54,14 @@ export class ComplaintComponent implements OnInit {
 	}
 
 	estimatedTimePopup() {
+		const defaultSpanValue: number = this.data.status === 'in progress' ? this.data.estimatedTime.value : null;
+		const defaultSpanType: string = this.data.status === 'in progress' ? this.data.estimatedTime.spanType : '';
+
+		this.estimatedTimeForm = new FormGroup({
+			value: new FormControl(defaultSpanValue, [Validators.required, Validators.min(1)]),
+			spanType: new FormControl(defaultSpanType, [Validators.required, this.verifySpanType.bind(this)])
+		});
+
 		this.showEstimatedTimeForm = true;
 	}
 
@@ -71,7 +72,6 @@ export class ComplaintComponent implements OnInit {
 	deleteComplaint() {
 		this.complaintApi.deleteComplaint(this.data._id).subscribe(
 			(data) => {
-				console.log(data);
 				this.reload.emit(true);
 			},
 			(err) => console.log(err)
@@ -86,27 +86,42 @@ export class ComplaintComponent implements OnInit {
 	statusChange(event: any) {
 		const currentStatus = event.target.value;
 		this.currentStatus = currentStatus;
+
 		if (currentStatus === 'in progress') this.estimatedTimePopup();
-		else this.updateStatus({
-			status: this.currentStatus
-		});
+		else if (currentStatus !== this.data.status) {
+			this.updateStatus({
+				status: this.currentStatus
+			});
+		}
 	}
 
 	updateEstimatedTime() {
 		if (this.estimatedTimeForm.valid) {
+			this.updatingStatus = true;
 			const update = {
 				status: this.currentStatus,
 				estimatedTime: this.estimatedTimeForm.value
 			};
 			this.updateStatus(update);
 		}
-		console.log(this.estimatedTimeForm.value, this.estimatedTimeForm.valid);
 	}
 
 	updateStatus(update: any) {
 		this.complaintApi.updateResolve(this.data._id, update).subscribe(
-			data => console.log(data),
-			err => console.log(err)
+			(data) => {
+				this.hideEstimatedTimePopup();
+				this.updatingStatus = false;
+
+				const updatedData = this.data;
+				updatedData.status = update.status;
+				if (update.estimatedTime) updatedData.estimatedTime = update.estimatedTime;
+
+				this.data = updatedData;
+			},
+			(err) => {
+				this.updatingStatusErr = true;
+				console.log(err);
+			}
 		);
 	}
 }
