3180bf2cd8a87eb0fa9188d7c196b6a40d1ab108
livingscripts <nilesh22.a67@gmail.com>
2020-07-03 22:24:59 +0530

    resolve board filters complete

--- src/app/components/resolve-board/resolve-board.component.ts
+++ src/app/components/resolve-board/resolve-board.component.ts
@@ -2,24 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { ComplaintComponent } from '../complaints/complaint/complaint.component';
 import { ComplaintapiService } from 'src/app/services/apis/complaintapi.service';
 import { LoginDataService } from 'src/app/services/dataServices/auth.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DepartmentapiService } from 'src/app/services/apis/departmentapi.service';
+import { Department } from 'src/app/models/department.model';
 
 @Component({
-  selector: 'ttnd-resolve-board',
-  templateUrl: './resolve-board.component.html',
-  styleUrls: ['./resolve-board.component.css']
+	selector: 'ttnd-resolve-board',
+	templateUrl: './resolve-board.component.html',
+	styleUrls: ['../common.css', './resolve-board.component.css']
 })
 export class ResolveBoardComponent implements OnInit {
-  readonly complaint = ComplaintComponent;
-  complaintFetchFilter: any = {};
+	readonly complaint = ComplaintComponent;
+	complaintFilter: any = {};
+	filtersForm: FormGroup = null;
+	departments: Array<Department> = [];
 
-  constructor(public complaintApi: ComplaintapiService, private authData: LoginDataService) { }
+	constructor(public complaintApi: ComplaintapiService, private deptService: DepartmentapiService) {}
 
-  ngOnInit(): void {
-    const userProfile = this.authData.idTokenData();
+	ngOnInit(): void {
+		this.deptService.getDepartments().subscribe(
+			(data) => (this.departments = data),
+			(err) => console.log(err)
+		);
 
-    this.complaintFetchFilter = {
-      email: userProfile.email
-    }
-  }
+		this.filtersForm = new FormGroup({
+			issueId: new FormControl(null),
+			department: new FormControl(''),
+			status: new FormControl('')
+		});
+	}
 
+	applyFilters() {
+		this.complaintFilter = Object.entries(this.filtersForm.value).reduce(
+			(a, [k, v]) => (v ? ((a[k] = v), a) : a),
+			{}
+		);
+	}
+
+	resetFilters() {
+		this.complaintFilter = {};
+	}
 }
