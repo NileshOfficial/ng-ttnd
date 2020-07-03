221876dc0b25af81ab10e4fb87a2335d10b4aa2f
livingscripts <nilesh22.a67@gmail.com>
2020-07-03 12:39:39 +0530

    added query filter to get request

--- src/app/services/apis/complaintapi.service.ts
+++ src/app/services/apis/complaintapi.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { COMPLAINT } from '../../config/uri.conf';
 import { Complaint } from 'src/app/models/complaint.model';
@@ -11,7 +11,13 @@ export class ComplaintapiService {
 	constructor(private http: HttpClient) {}
 
 	get(query: any = {}, skip: number = 0, limit: number = 0): Observable<Array<Complaint>> {
-		return this.http.get<Array<Complaint>>(COMPLAINT);
+		const constructedQuery = {
+			...query,
+			...(skip && { skip: skip }),
+			...(limit && { limit: limit })
+		};
+		const params = new HttpParams({ fromObject: constructedQuery });
+		return this.http.get<Array<Complaint>>(COMPLAINT, { params: params });
 	}
 
 	postComplaint(data: any): Observable<{ inserted: boolean; referenceToken: string }> {
