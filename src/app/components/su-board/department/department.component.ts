adf2bca643de6b4530ec4109813bb6ead694698e
livingscripts <nilesh22.a67@gmail.com>
2020-07-04 14:38:50 +0530

    edit department layout complete

--- src/app/components/su-board/department/department.component.ts
+++ src/app/components/su-board/department/department.component.ts
@@ -1,28 +1,48 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Department } from '../../../models/department.model';
 import { DepartmentapiService } from 'src/app/services/apis/departmentapi.service';
 
 @Component({
-  selector: 'ttnd-department',
-  templateUrl: './department.component.html',
-  styleUrls: ['../../common.css', './department.component.css']
+	selector: 'ttnd-department',
+	templateUrl: './department.component.html',
+	styleUrls: ['../../common.css', './department.component.css']
 })
 export class DepartmentComponent implements OnInit {
+	deparmentEditInput: ElementRef;
 
-  @Input() data: Department = null;
-  @Output() reload: EventEmitter<boolean> = new EventEmitter();
+	@ViewChild('dept') set content(content: ElementRef) {
+		if (content) {
+      this.deparmentEditInput = content;
+      this.deparmentEditInput.nativeElement.focus();
+			this.deparmentEditInput.nativeElement.value = this.data.name;
+		}
+	}
 
-  constructor(private deptApi: DepartmentapiService) { }
+	@Input() data: Department = null;
+	@Output() reload: EventEmitter<boolean> = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+	editingDept: boolean = false;
 
-  deleteDept() {
-    this.deptApi.deleteDeparment(this.data._id).subscribe(data => {
-      this.reload.emit(true);
-    }, err => {
-      console.log(err);
-    });
-  }
+	constructor(private deptApi: DepartmentapiService) {}
 
+	ngOnInit(): void {}
+
+	editDepartment() {
+		this.editingDept = true;
+	}
+
+	hideEditInput() {
+		this.editingDept = false;
+	}
+
+	deleteDept() {
+		this.deptApi.deleteDeparment(this.data._id).subscribe(
+			(data) => {
+				this.reload.emit(true);
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
+	}
 }
