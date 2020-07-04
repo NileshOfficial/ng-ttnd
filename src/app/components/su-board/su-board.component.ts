5715668890ad1de821e9d2adf9537f0a618c4189
livingscripts <nilesh22.a67@gmail.com>
2020-07-04 15:34:59 +0530

    add new department done

--- src/app/components/su-board/su-board.component.ts
+++ src/app/components/su-board/su-board.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentComponent } from './department/department.component';
 import { DepartmentapiService } from 'src/app/services/apis/departmentapi.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'ttnd-su-board',
-  templateUrl: './su-board.component.html',
-  styleUrls: ['../common.css', './su-board.component.css']
+	selector: 'ttnd-su-board',
+	templateUrl: './su-board.component.html',
+	styleUrls: ['../common.css', './su-board.component.css']
 })
 export class SuBoardComponent implements OnInit {
-  readonly department = DepartmentComponent;
+	readonly department = DepartmentComponent;
+	addDepartmentForm: FormGroup;
 
-  constructor(public deptApi: DepartmentapiService) { }
+	constructor(public deptApi: DepartmentapiService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.addDepartmentForm = new FormGroup({
+			name: new FormControl('', [Validators.required])
+		});
+	}
 
+	addNewDept() {
+		if (this.addDepartmentForm.valid)
+			this.deptApi.addDepartment(this.addDepartmentForm.value).subscribe(
+				(data) => {
+					console.log(data);
+				},
+				(err) => {
+					console.log(err);
+				}
+			);
+	}
 }
