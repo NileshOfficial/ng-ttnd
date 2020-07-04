d9935a583bd0c8079524dfbe7e012ec9e090e67d
livingscripts <nilesh22.a67@gmail.com>
2020-07-04 13:52:39 +0530

    delete department completet

--- src/app/components/su-board/department/department.component.ts
+++ src/app/components/su-board/department/department.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Department } from '../../../models/department.model';
+import { DepartmentapiService } from 'src/app/services/apis/departmentapi.service';
 
 @Component({
   selector: 'ttnd-department',
@@ -11,9 +12,17 @@ export class DepartmentComponent implements OnInit {
   @Input() data: Department = null;
   @Output() reload: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor(private deptApi: DepartmentapiService) { }
 
   ngOnInit(): void {
+  }
+
+  deleteDept() {
+    this.deptApi.deleteDeparment(this.data._id).subscribe(data => {
+      this.reload.emit(true);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
