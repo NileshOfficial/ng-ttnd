6feb8a904d106a7355b846abdea52e020e3de1e5
livingscripts <nilesh22.a67@gmail.com>
2020-07-06 20:55:01 +0530

    showing navlinks based on user role

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { LoginDataService } from 'src/app/services/dataServices/auth.service';
 
 @Component({
-  selector: 'ttnd-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css', '../common.css']
+	selector: 'ttnd-nav',
+	templateUrl: './nav.component.html',
+	styleUrls: ['./nav.component.css', '../common.css']
 })
 export class NavComponent implements OnInit {
+	userEmail: string = '';
+	roleCode: number = null;
+	constructor(private authData: LoginDataService) {}
 
-  userEmail: string = '';
-  constructor(private authData: LoginDataService) {}
-
-  ngOnInit(): void {
-    this.userEmail = this.authData.idTokenData().email;
-  }
-
+	ngOnInit(): void {
+    const idTokenData = this.authData.idTokenData();
+    console.log(idTokenData.role_code)
+		this.userEmail = idTokenData.email;
+		this.roleCode = idTokenData.role_code;
+	}
 }
